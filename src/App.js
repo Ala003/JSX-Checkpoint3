490c7d15f01d09645c005c6ba39344d307f34913
Ala003 <Alaeddine.Salmouna@essths.u-sousse.tn>
2021-03-06 15:11:22 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React from 'react' ;  
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+<>
+
+  <div class="a" >
+                  <h1 className="title red">Ala</h1>
+    <img src="/imageInSrc.jpg" />
+    <br/>
+    <img src=".public/imageInPublic.jpg" />
+    <br/>
+    <video width={320} height={240} controls>
+    <source src="myVideo.mp4" type="video/mp4" />
+  </video>
+  </div>
+ 
+</>)
+};
+
 
 export default App;
+
